587f697f2cb7fa992aff40f34d52d4fa324c37ef
Juan Nava <juan.ns17@gmail.com>
2020-07-29 16:53:01 +0000

    added messages upon establishing connection with server

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
+    host: '135.23.222.131',
     port: 50541
   });
   // interpret incoming data as text
@@ -10,6 +10,13 @@ const connect = function() {
 
   conn.on('data', (data) => {
     console.log(data);
+  });
+
+  conn.on('connect', (data) => {
+    console.log('Successfuly connected to game server');
+
+    conn.write('Name: SNK');
+
   });
   
   
