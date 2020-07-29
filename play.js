83bbd91a97c2eea538784b7f7968bc06fe815bec
Juan Nava <juan.ns17@gmail.com>
2020-07-29 18:17:50 +0000

    added user input functionality and handling exit command

--- play.js
+++ play.js
@@ -1,8 +1,36 @@
 //const net = require('net');
 
 // PLAY
-
 const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  // const handleUserInput =  (stdin.on('data', (key) => {
+  //   if (key === '\u0003') {
+  //     process.exit();
+  // }
+  // };
+
+  const handleUserInput = stdin.on('data', (key) => {	
+    if (key === '\u0003') {	
+      process.exit();	
+    }	
+  })
+
+  return stdin;
+}
+
+
+setupInput();
